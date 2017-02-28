3627f0a8ba9b6b250740a8ac2c0a93da1259de87
SeanPianka <me@seanpianka.com>
2017-02-28 15:38:43 -0500

    added sections for website

---
+++ vulnbid/auth/forms.py
@@ -0,0 +1,42 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField, PasswordField
+from wtforms.validators import InputRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField(
+        'username',
+        validators=[
+            InputRequired("Enter your username.")
+        ]
+    )
+    password = PasswordField(
+        'password',
+        validators=[
+            InputRequired("Enter your password.")
+        ]
+    )
+    remember_me = BooleanField(
+        'remember_me'
+    )
+    submit = SubmitField(
+        'login'
+    )
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(
+        'username',
+        validators=[
+            InputRequired("Enter your username.")
+        ]
+    )
+    password = PasswordField(
+        'password',
+        validators=[
+            InputRequired("Enter your password.")
+        ]
+    )
+    submit = SubmitField(
+        'login'
+    )
